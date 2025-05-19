707bacc203fba88cd2d67d681136aef5ba5e6804
Jacob Brewer <Jacob.brewer@bthree.uk>
2025-05-19 16:37:01 +0100

    adding pem setup with vault

--- cmd/kube-mutating-webhook/main.go
+++ cmd/kube-mutating-webhook/main.go
@@ -1,7 +1,137 @@
 package main
 
-import "fmt"
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"log/slog"
+	"net/http"
+
+	"github.com/jacobbrewer1/web"
+	"github.com/jacobbrewer1/web/logging"
+)
+
+const (
+	// appName is the name of the application.
+	appName = "kube-mutating-webhook"
+)
+
+type (
+	// AppConfig is the configuration for the app.
+	AppConfig struct {
+		// pemCert is the PEM certificate for the webhook.
+		PemCert []byte
+
+		// pemKey is the PEM key for the webhook.
+		PemKey []byte
+
+		PemCA []byte
+	}
+
+	// App is the main application struct.
+	App struct {
+		// base is the base web application.
+		base *web.App
+
+		// config is the application configuration.
+		config *AppConfig
+
+		// certPool is the certificate pool for the webhook.
+		certPool *x509.CertPool
+	}
+)
+
+// NewApp creates a new App instance with the given logger.
+func NewApp(l *slog.Logger) (*App, error) {
+	base, err := web.NewApp(l)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create base app: %w", err)
+	}
+
+	cfg := new(AppConfig)
+
+	return &App{
+		base:     base,
+		config:   cfg,
+		certPool: x509.NewCertPool(),
+	}, nil
+}
+
+// Start initializes the app and starts the base application.
+func (a *App) Start() error {
+	if err := a.base.Start(
+		web.WithViperConfig(),
+		web.WithConfigWatchers(func() {
+			a.Shutdown()
+		}),
+		web.WithVaultClient(),
+		web.WithInClusterKubeClient(),
+	); err != nil {
+		return fmt.Errorf("failed to start base app: %w", err)
+	}
+
+	server, err := a.buildSecureServer()
+	if err != nil {
+		return fmt.Errorf("failed to build secure server: %w", err)
+	}
+
+	if err := a.base.StartServer("webhook-server", server); err != nil {
+		return fmt.Errorf("failed to start webhook server: %w", err)
+	}
+
+	return nil
+}
+
+func (a *App) buildSecureServer() (*http.Server, error) {
+	// Load the PEM certificate and key from the config.
+	if a.config.PemCert == nil || a.config.PemKey == nil {
+		return nil, fmt.Errorf("PEM certificate or key not found")
+	}
+
+	// Add the PEM certificate to the cert pool.
+	if !a.certPool.AppendCertsFromPEM(a.config.PemCA) {
+		return nil, fmt.Errorf("failed to append PEM certificate to cert pool")
+	}
+
+	tlsCert, err := tls.X509KeyPair(a.config.PemCert, a.config.PemKey)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load PEM certificate and key: %w", err)
+	}
+
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{tlsCert},
+		ClientCAs:    a.certPool,
+	}
+
+	server := &http.Server{
+		Addr:      ":8443",
+		TLSConfig: tlsConfig,
+	}
+
+	return server, nil
+}
+
+// WaitForEnd waits for the application to finish.
+func (a *App) WaitForEnd() {
+	a.base.WaitForEnd(a.Shutdown)
+}
+
+// Shutdown is called to shut down all running services.
+func (a *App) Shutdown() {
+	a.base.Shutdown()
+}
 
 func main() {
-	fmt.Println("Mutating Webhook Server")
+	l := logging.NewLogger(
+		logging.WithAppName(appName),
+	)
+
+	app, err := NewApp(l)
+	if err != nil {
+		panic(fmt.Errorf("failed to create app: %w", err))
+	}
+
+	if err := app.Start(); err != nil {
+		panic(fmt.Errorf("failed to start app: %w", err))
+	}
 }
---
+++ cmd/kube-mutating-webhook/pem.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"context"
+	"crypto/x509"
+	"errors"
+	"fmt"
+	"log/slog"
+	"time"
+
+	"github.com/jacobbrewer1/vaulty"
+	"github.com/jacobbrewer1/web"
+	"github.com/jacobbrewer1/web/k8s"
+)
+
+func (a *App) waitForPemExpiry(l *slog.Logger) web.AsyncTaskFunc {
+	return func(ctx context.Context) {
+		l.Info("monitoring pem expiry")
+
+		ticker := time.NewTicker(15 * time.Second)
+		defer ticker.Stop()
+
+		for {
+			select {
+			case <-ticker.C:
+			}
+		}
+	}
+}
+
+func (a *App) reloadPemIfNeeded(
+	ctx context.Context,
+	l *slog.Logger,
+	vaultClient vaulty.Client,
+	pemPath string,
+	refreshThreshold time.Duration,
+) error {
+	// Get the current pem certificate
+	currentPemCert := a.config.PemCert
+	currentPemKey := a.config.PemKey
+	currentPemCA := a.config.PemCA
+	if currentPemCert == nil || currentPemKey == nil || currentPemCA == nil {
+		return errors.New("pem certificate not found")
+	}
+
+	// When does the pem expire?
+	cert, err := x509.ParseCertificate(currentPemCert)
+	if err != nil {
+		return fmt.Errorf("failed to parse pem certificate: %w", err)
+	}
+
+	// Check if the certificate is about to expire
+	if time.Until(cert.NotAfter) >= refreshThreshold {
+		l.Debug("pem certificate is still valid")
+		return nil
+	}
+
+	l.Info("pem certificate is about to expire, reloading")
+	if err := a.loadNewPem(ctx, vaultClient, pemPath); err != nil {
+		return fmt.Errorf("failed to reload pem certificate: %w", err)
+	}
+	l.Info("pem certificate reloaded successfully")
+
+	return nil
+}
+
+func (a *App) loadNewPem(
+	ctx context.Context,
+	vaultClient vaulty.Client,
+	pemPath string,
+) error {
+	vc := vaultClient.Client()
+
+	secret, err := vc.Logical().WriteWithContext(ctx, pemPath, map[string]any{
+		"common_name": fmt.Sprintf("%s.%s.svc", appName, k8s.DeployedNamespace()),
+		"ttl":         "24h",
+	})
+	if err != nil {
+		return fmt.Errorf("failed to write pem secret: %w", err)
+	}
+
+	pemCert, ok := secret.Data["pem_cert"].(string)
+	if !ok {
+		return errors.New("pem_cert not found in secret data")
+	}
+
+	pemKey, ok := secret.Data["pem_key"].(string)
+	if !ok {
+		return errors.New("pem_key not found in secret data")
+	}
+
+	ca, ok := secret.Data["issuing_ca"].(string)
+
+	a.config.PemCert = []byte(pemCert)
+	a.config.PemKey = []byte(pemKey)
+	a.config.PemCA = []byte(ca)
+
+	return nil
+}
